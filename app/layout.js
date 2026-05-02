18573a5435e6910a39bc9b8ec925b0d044a45384
Jake Samuels <samuels242@googlemail.com>
2026-05-02 14:08:22 +0100

    Cleanup files

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "@/components/Header";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,8 +9,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
