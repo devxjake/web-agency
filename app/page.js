a50f9c964e0ba76f3b5b1370861e71dfa72bac41
Jake Samuels <samuels242@googlemail.com>
2026-05-03 22:27:12 +0100

    Import Feature marquee

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import FeatureMarquee from "@/components/FeatureMarquee";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <FeatureMarquee />
     </main>
   );
 }
